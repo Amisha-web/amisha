ebe75d2aa6519eeb802f20922026e97e0998a56d
Tomasz Potanski <tomasz@potanski.pl>
2013-11-19 18:48:43 +0100

    modifications

--- README.md
+++ README.md
@@ -2,9 +2,13 @@ sudoku
 ======
 Sudoku javascript web app
 
+
 Technologies: JavaScript, HTML5, CSS3, Twitter Bootstrap, AJAX, jQuery
+
 Link: http://potanski.pl/projects/sudoku/
+
 Date: March, 2013
+
 Sudoku web game built with JavaScript with cool & crazy interface. User is able to undo/redo moves, get
 hints, see solving time, pause the game, save game state to web storage (and later load from it). Above 10
 different boards.
--- scripts.js
+++ scripts.js
@@ -8,16 +8,11 @@
 			var plansze = new Array();
 			var minuty = 0;
 
-			//var undoManager;
-
-				var undoManager;
+			var undoManager;
 					
-				//var circleDrawer = new CircleDrawer('view');
-				//circleDrawer.setUndoManager(undoManager);
                 
-				var btnUndo = document.getElementById('undo');
-				var btnRedo = document.getElementById('redo');
-				//var btnClear = document.getElementById('btnClear');
+			var btnUndo = document.getElementById('undo');
+			var btnRedo = document.getElementById('redo');
 
 			var opisPlanszy = //board description
 				"020730001" +
@@ -157,7 +152,6 @@
 			akt_plansza = opisPlanszy;
 			var n = [];
 			for (var t = 0 ; t < akt_plansza.length ; t++){
-				//akt_plansza.forEach(function(e) { n.push(e); });
 				n.push(akt_plansza[t]);
 			}
 			akt_planszaChar = n;
@@ -167,7 +161,7 @@
 			* Function changes the current board to the next one.
 			*/
 			function plansza_dalej(){ //next board
-				var r=confirm("Czy na pewno chcesz zmienic plansze? \nStracisz wszystko, co zrobiles do tej pory");
+				var r=confirm("Do you really want to change board? \nBy doing it you will loose everything you have done already!");
 				if (r==true){
 					numer_planszy++;
 					if (numer_planszy > 10){
@@ -178,7 +172,6 @@
 					document.getElementById('nr_nr').value = numer_planszy;
 
 					for (var t = 0 ; t < akt_plansza.length ; t++){
-					//akt_plansza.forEach(function(e) { n.push(e); });
 						n.push(akt_plansza[t]);
 					}
 					akt_planszaChar = n;
@@ -194,7 +187,6 @@
 	
 					undoManager.clear();
 					updateUI();
-					//odliczaj(document.getElementById('sekundy'));
 	
 					var x = document.getElementsByTagName('link');
 					var queryString = '?reload=' + new Date().getTime();
@@ -221,7 +213,6 @@
 				var r=confirm("Czy na pewno chcesz zmienic plansze? \nStracisz wszystko, co zrobiles do tej pory");
 				if (r==true){
 					numer_planszy--;
-					//alert('-');
 					if (numer_planszy < 0){
 						numer_planszy = 10;
 					}
@@ -230,7 +221,6 @@
 					document.getElementById('nr_nr').value = numer_planszy;
 	
 					for (var t = 0 ; t < akt_plansza.length ; t++){
-						//akt_plansza.forEach(function(e) { n.push(e); });
 						n.push(akt_plansza[t]);
 					}
 					akt_planszaChar = n;
@@ -255,7 +245,6 @@
 							x[a].setAttribute('href', qs.replace(/\?.*|$/, queryString));
 						}
 					}	
-					//odliczaj(document.getElementById('sekundy'));
 				}
 				else
 				{
@@ -266,7 +255,6 @@
 			spauzowane = false;
 			minuty = 0;
 			sek = 0;
-			//o=document.getElementById('sekundy')
 
 
 			/**
@@ -286,7 +274,7 @@
 						o.innerHTML=minuty + ':0'+sek;	
 					}
 				}
-				//[b]if(sek==5)alert('zostalo 5 sekund')[/b]
+
 				if(sek==59){
 					sek = -1;
 					minuty++;
@@ -323,9 +311,9 @@
 			* Function gives coordinates of i-th square of the board
 			*/
 			function wspolrzedne(i){ //coordinates of ...
-				if (i < 0 || i > 80) {
+				//if (i < 0 || i > 80) {
 					//alert("wspolrzedne: zle dane");
-				}
+				//}
 				return Math.floor(i/9) + 10 * (i % 9) + 11;
 			}
 
@@ -340,9 +328,9 @@
 			* Function returns an array of numbers of squares in one raw
 			*/
 			function wiersz(i){ //row
-				if (i < 1 || i > 9) {
+				//if (i < 1 || i > 9) {
 					//alert("wiersz: Zly wiersz");
-				}
+				//}
 				w = new Array();
 				for(k = 10; k <= 90; k+=10){
 					w.push(k+i);
@@ -354,9 +342,9 @@
 			* Function returns an array of numbers of squares in one column
 			*/
 			function kolumna(i){ //column
-				if (i < 1 || i > 9){
+				//if (i < 1 || i > 9){
 					//alert("kolumna: Zly argument");
-				}
+				//}
 				w = new Array();
 				for(k = 1; k <= 9; k++){
 					w.push(i*10 + k);
@@ -377,15 +365,11 @@
 			* Function chceks if there are duplicates in the board
 			*/
 			function sprawdz_duplikatyTablice(t, a){ 
-				//debug += 'sprawdzam<br/>';
 				var vv = $('#i' + a).val();
-				//alert('wart. wpisana: '+vv);
 				debug +='vv: '+vv+', a: '+a+'<br/>';
 				for(i = 0; i < 9; i++){
 					var v = $('#i' + t[i]).val();
 					debug +='v: '+v+', t[i]: '+t[i]+'<br/>';
-					//alert('wart. wysw.: '+v);
-					//$('#td'+t[i]).css('background-color', '#7cfc00');
 					if (t[i] != a && v == vv && v != ''){
 						debug+='<span class="czerwony">konflikt: '+'v: '+v+', vv: '+vv+',t[i]: '+t[i]+', a: '+a+'</span><br/>';
 						$('#td'+t[i]).css('background-color', 'red');
@@ -395,12 +379,10 @@
 			}
 
 			function podswietlTablice_mycha(t){
-				//alert(t[0]);
 				$('#td'+t[0]+t[1]).css('background-color', '#f5deb3');
 			}
 
 			function podswietlTablice_mycha_norma(t){
-				//alert(t[0]);
 				$('#td'+t[0]+t[1]).css('background-color', 'transparent');
 			}
 
@@ -417,33 +399,21 @@
 			* Function is responsible for the hint functionallity
 			*/
 			function podpowiedz(nr){ 
-				//alert('podpowiedz dla pola: '+nr);
 				var w = new Array();
 				debug = 'podpwiedz: <br/>';
 	
 				wwiersz = new Array();
 				kkolumna = new Array();
 				kkwadrat = new Array();
-				/*
-				podswietlTablice(wiersz(i % 10));
-				podswietlTablice(kolumna(Math.floor(i/10)));
-				//podswietlTablice(kwadrat(1 + Math.floor((i-10)/30) + 3 * Math.floor(((i % 10)-1)/3)));
-				*/
+	
 	
 				wwiersz = wiersz(nr % 10);
 				kkolumna = kolumna(Math.floor(nr/10));
 				kkwadrat = kwadrat(1 + Math.floor((nr-10)/30) + 3 * Math.floor(((nr % 10)-1)/3));
-				//alert('poza111111?');
-				//w.push(i*10 + k);
+
 				for (var i = 1; i <= 9; i++){
-					//alert('i in y bed');
-					//debug += 'obrabiam: '+i+'<br/>';
-					//$("#debug").html(debug);
 					var czy_jest = false;
 					for (var j = 0; j < 9 && !czy_jest; j++){
-						//if (wartosc_z_nr(wiersz[j]) == 2){
-							//debug += 'w wierszu mam '+wartosc_z_nr(wwiersz[j])+', obrabiam nr: '+wwiersz[j]+'<br/>';
-						//}
 						if (i == wartosc_z_nr(wwiersz[j]) && wwiersz[j] != nr){
 							czy_jest = true;
 							break;
@@ -471,16 +441,15 @@
 				
 				}
 				if (w.length == 0){
-					alert('system nie byl w stanie wygenerowac podpowiedzi dla tego pola');
+					alert('System was not able to generate a hint ;-/');
 				} else {
-					tresc = 'Mala podpowiedz: \n';
+					tresc = 'A little hint: \n';
 					tresc += w[0];
 					for (var i = 1 ; i < w.length ; i++ ){
 						tresc += ', '+ w[i];
 					}
 					alert(tresc);
 				}
-				//alert('poza?');
 				$("#debug").html(debug);
 			}
 
@@ -488,21 +457,15 @@
 				podswietlTablice(wiersz(i % 10));
 				podswietlTablice(kolumna(Math.floor(i/10)));
 
-				//alert('22wejscie z nr: '+i)
-				//podswietlTablice(kwadrat(1 + Math.floor((i-10)/30) + 3 * Math.floor(((i % 10)-1)/3)));
 			}
 
 			/**
 			* Function checks if selected square number has value that is already on the board
 			*/
 			function sprawdz_duplikaty(i){ //check for duplicates
-				//alert('sprawdzam!');
 
-				debug = 'wiersz<br/>';
 				sprawdz_duplikatyTablice(wiersz(i % 10), i);
-				debug += 'kolumna<br/>';
 				sprawdz_duplikatyTablice(kolumna(Math.floor(i/10)), i);
-				debug += 'kwadrat<br/>';
 				sprawdz_duplikatyTablice(kwadrat(1 + Math.floor((i-10)/30) + 3 * Math.floor(((i % 10)-1)/3)), i);
 				$("#debug").html(debug);
 			}
@@ -518,9 +481,9 @@
 			* Function returns an array of numbers of squares in one raw
 			*/
 			function kwadrat(i){ //square
-				if (i < 1 || i > 9){
+				//if (i < 1 || i > 9){
 					//alert("kwadrat: Zly argument");
-				}
+				//}
 				w = new Array();
 				x = ((i - 1) % 3) * 3 + 1;
 				y = (Math.floor((i - 1) / 3)) * 3 + 1;
@@ -609,23 +572,11 @@
 			* Function changes the value of selected board
 			*/
 			function zmien_wartosc_pola(nr, na_co){ //change the value of a square
-				//$('#i' + nr).val() = 7;
-				//-----------------alert('pole: '+nr+' na: '+na_co);
-				//if (na_co == ''){
-					//	alert('ten przyp');
-					//	$('#i'+nr).attr('value', ' ');
-				//} else {
-					//$('#i'+nr).attr('value', na_co);
 					$('#i'+nr).val(na_co);
-				//}
-	
-				//alert('cos');
-				//$('#i' + nr).val() = na_co;
 			}
 
 			function wejscie(nr){ //enter
 				//TO NIE DZIALA
-				//alert('wejscie z nr: '+nr);
 			}
 
 			function czysc_przeszlosc(){ //clear history
@@ -634,33 +585,14 @@
 			}
 
 			function iwentuj(){
-	
-				//zmien_wartosc_pola(11,888);
-
-				/*$(".field2 input").click(function(event){
-					alert('adsfadsfads');
-					last_wart_in = wartosc_z_nr(last);
-					alert('in: '+last_wart_in);
-				});*/
-
-				/*$(".field2 input").click(function(event){
-					alert("sdklfjsdkfljsd");
-				});*/
 					
 				$(".field input").focus(function(){
-					//llll = last;
 					last = $(this).parent().attr('id').substr(2,2);
-					//last_wart_in = wartosc_z_nr(last);
-					//alert('in: '+last_wart_in);
 					podswietl(last); //highlight
 					last_wart_in = wartosc_z_nr(last);
-					//last_wart_in = 7;
-					//last_wart_in = wartosc_z_nr(last);
-					//alert('in: '+last_wart_in);
 
 					if (last_konflikt != -8){
 						sprawdz_duplikaty(last_konflikt); //check for duplicates
-						//undoManager.register(undefined, removeCircle, [id], 'Remove circle', undefined, createCircle, [id, x, y, color], 'Create circle');
 					}
 
 				});
@@ -671,62 +603,16 @@
 					last_wart_out = wartosc_z_nr(last_konflikt);
 					last_last = $(this).parent().attr('id').substr(2,2);
 
-					//alert('out: '+last_wart_out+'\nin: '+last_wart_in);
-
 					if (last_wart_in != last_wart_out && last_wart_out != -1 && last_wart_in != -1){
-						//alert('manager-register');
 						var ident = last +''+last_wart_in+''+last_wart_out;
 						if (last_update != ident){
 							last_update = ident;
 							undoManager.register(undefined, zmien_wartosc_pola, [last_last, last_wart_in], 'undo zmiane wartosci', undefined, zmien_wartosc_pola, [last_last, last_wart_out], 'redo zmiane wartosci');
-							//alert('manager-register\nin: '+last_wart_in+'\nout: '+last_wart_out+'\nlast: '+last+'\nlast_last: '+last_last);
-							//zmien_wartosc_pola(last_last, last_wart_in);
 						}
-						/*
-						kkolumna = kolumna(Math.floor(nr/10));
-						kkwadrat = kwadrat(1 + Math.floor((nr-10)/30) + 3 * Math.floor(((nr % 10)-1)/3));
-						*/
-						//alert('zmieniam z: '+akt_plansza[9*(last_last % 10) + Math.floor(last_last/10)]);
 				
-				
-						/*cos = '';
-						for (var k = 0 ; k < 3 ; k++){
-							for (var z = 1 ; z <= 3 ; z++){
-								cos += akt_planszaChar[k*9 + z -1];
-								//akt_planszaChar[k*9 + z -1] = 7;
-							}
-							cos += '\n';
-						}
-						alert(cos);*/
-						//alert('last_last: '+last_last+'\n'+((last_last % 10)-1) + '\n'+ Math.floor(last_last/10));
-						//alert(akt_planszaChar[9*((last_last % 10)-1) + Math.floor(last_last/10) -1]);
-						akt_planszaChar[9*((last_last % 10)-1) + Math.floor(last_last/10) -1]=last_wart_out;
-						//akt_planszaChar[0]=8;
-						//alert(akt_planszaChar[9*((last_last % 10)-1) + Math.floor(last_last/10) -1]);
-						/*cos = '';
-						for (var k = 0 ; k < 3 ; k++){
-							for (var z = 1 ; z <= 3 ; z++){
-								cos += akt_planszaChar[k*9 + z -1]
-							}
-							cos += '\n';
-						}
-						alert(cos);*/
-					
-						/*cos = '';
-						for (var i = 0 ; i < 81 ; i++){
-							cos += akt_planszaChar[i];
-							if ((i+1) % 3 == 0){
-								cos += '|';
-							}
-							if ((i+1) % 9 == 0){
-									cos += '\n';
-							}	
-						}
-						alert(cos);*/
-	
+						akt_planszaChar[9*((last_last % 10)-1) + Math.floor(last_last/10) -1]=last_wart_out;	
 					}
-	
-					//alert('out: '+last_wart_out+'\nin: '+last_wart_in);
+
 
 				});
 
@@ -758,9 +644,9 @@
 	
 				$("#sprawdz").click(function(event){
 					if (poprawneRozwiazanie()){
-						alert("Dobrze");
+						alert("Good! Grats!");
 					} else {
-						alert("Niedobrze");
+						alert("Not good! ;-/");
 					}
 				});
 
@@ -781,13 +667,11 @@
 					localStorage.setItem('minuty', minuty); //minutes
 					localStorage.setItem('sek', sek); //seconds
 					localStorage.setItem('numer_planszy', numer_planszy); //board number
-	
-					//alert('cos pisze');
 				});
 
 				$("#load").click(function(event){
 					if (localStorage.getItem('czy_juz_byl_save') == 'tak'){
-						var r=confirm("Czy na pewno chcesz wczytac plansze? Stracisz wszysto, co teraz ugrales, jesli nie zapiszesz");
+						var r=confirm("Do you really want to load board? If you do not save, you will loose everything you made!");
 						if (r==true){
 							akt_planszaChar2 = localStorage.getItem('save_planszy');
 							length = localStorage.getItem('rozmiar_save_planszy');
@@ -798,41 +682,9 @@
 								}
 							}
 							akt_planszaChar = n;
-				
-							/*cos = '';
-							for (var i = 0 ; i < 20 ; i ++){
-								cos += akt_planszaChar[i];
-							}
-							alert(cos);*/
-							//alert(length);
-							//akt_planszaChar = 
-							/*var n = [];
-							for (var i = 0 ; i < 2*length ; i++){
-								if (i % 2 == 0){
-									n.push(akt_planszaChar2[i]);
-								}
-							}
-							akt_planszaChar = n;*/
 					
-							//akt_plansza = akt_planszaChar.toString();
 							akt_plansza = akt_planszaChar.join(""); //actual board char
 			
-							/*	cos = '';
-							for (var i = 0 ; i < 81 ; i++){
-								cos += akt_planszaChar[i];
-								if ((i+1) % 3 == 0){
-									cos += '|';
-								}
-								if ((i+1) % 9 == 0){
-									cos += '\n';
-								}
-							}
-							alert(cos);*/
-			
-			
-							//akt_plansza=akt_plansza.replace(",","");
-							//alert(akt_plansza.length);
-							//abc = akt_planszaChar.toString();
 							minuty = localStorage.getItem('minuty');
 							sek = localStorage.getItem('sek');
 							numer_planszy = localStorage.getItem('numer_planszy'); //board number
@@ -841,7 +693,7 @@
 		
 							last = -8;
 							last_konflikt = -8; //last conflict
-							//alert(abc.length);
+
 							$("#plansza").html(Plansza(akt_plansza));
 							$("table").css('border-spacing', '0');
 							rysujKrawedzie(); //draw boarders
@@ -862,8 +714,6 @@
 				}
 
 				$("#pauza").click(function(event){
-					//podpowiedz(last);
-					//alert('adsfasdfsa');
 					spauzowane = !spauzowane;
 
 					if (spauzowane){
@@ -871,9 +721,6 @@
 						$("#plansza").css('visibility', 'hidden'); //board
 					} else {
 						$("#pauza").html('Pauza');
-						//alert('ognia!');
-						//setTimeout(function(){odliczaj(document.getElementById('sekundy'),++sek)},1e3);
-						//++sek;
 						odliczaj(document.getElementById('sekundy')); //countdown(seconds)
 						$("#plansza").css('visibility', 'visible'); //board
 					}
@@ -883,15 +730,11 @@
 	
 				//---UNDO-REDO
 	
-		
 				undoManager = new UndoManager();
 					
-				//var circleDrawer = new CircleDrawer('view');
-				//circleDrawer.setUndoManager(undoManager);
                 
 				var btnUndo = document.getElementById('undo');
 				var btnRedo = document.getElementById('redo');
-				//var btnClear = document.getElementById('btnClear');
                 
 				function updateUI() {
 					btnUndo.disabled = !undoManager.hasUndo();
@@ -907,10 +750,6 @@
 					undoManager.redo();
 					updateUI();
 				};
-				/*btnClear.onclick = function() {
-					undoManager.clear();
-					updateUI();
-				};*/
                 
 				updateUI();
 	
